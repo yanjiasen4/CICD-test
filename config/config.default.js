369f5b660f339bb38f7d909661cad744f5626afe
yanjiasen4 <13122169550@163.com>
2018-06-27 19:18:18 +0800

    fix config bugs

--- config/config.default.js
+++ config/config.default.js
@@ -1,7 +1,16 @@
 'use strict';
 
 module.exports = appInfo => {
-  const config = exports = {};
+  const config = exports = {
+    alinode = {
+      appid: '54171',
+      secret: 'c308e971092ae7d64c2640430c9ecc2ab6fdcf1e',
+      logdir: '/tmp/',
+      error_log: [
+        '/root/.logs/error.#YYYY#-#MM#-#DD#.log'
+      ]
+    }
+  };
 
   // use for cookie sign key, should change to your own and keep security
   config.keys = appInfo.name + '_1529930826341_1425';
@@ -12,12 +21,3 @@ module.exports = appInfo => {
   return config;
 };
 
-exports.alinode = {
-  server: 'http://http://107.191.58.36:7001/',
-  appid: '54171',
-  secret: 'c308e971092ae7d64c2640430c9ecc2ab6fdcf1e',
-  logdir: '/tmp/',
-  error_log: [
-    '/root/.logs/error.#YYYY#-#MM#-#DD#.log'
-  ]
-};
