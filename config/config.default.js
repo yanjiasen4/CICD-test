d272297468d37c97b15299253768ea06a684f998
yanjiasen4 <13122169550@163.com>
2018-06-27 19:51:59 +0800

    add package.json config

--- config/config.default.js
+++ config/config.default.js
@@ -1,11 +1,16 @@
 'use strict';
 
+const path = require('path');
+
 module.exports = appInfo => {
   const config = exports = {};
 
   exports.alinode = {
     appid: '54171',
-    secret: 'c308e971092ae7d64c2640430c9ecc2ab6fdcf1e'
+    secret: 'c308e971092ae7d64c2640430c9ecc2ab6fdcf1e',
+    packages: [
+      path.join(appInfo.baseDir, 'package.json'),
+    ]
   };
 
   // use for cookie sign key, should change to your own and keep security
