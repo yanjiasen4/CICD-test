f8f82fc69e2c50838bb06184857ac075ed22d870
yanjiasen4 <13122169550@163.com>
2018-06-27 12:47:45 +0800

    test commit

--- test/app/controller/home.test.js
+++ test/app/controller/home.test.js
@@ -15,7 +15,7 @@ describe('test/app/controller/home.test.js', () => {
   it('should GET /', () => {
     return app.httpRequest()
       .get('/')
-      .expect('hi, world!')
+      .expect('hi, world')
       .expect(200);
   });
 });
