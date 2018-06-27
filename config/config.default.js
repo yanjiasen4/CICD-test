32f9507df816c9bef7c19bf62c3a5f10ef954590
yanjiasen4 <13122169550@163.com>
2018-06-27 18:53:51 +0800

    alinode monitor

--- config/config.default.js
+++ config/config.default.js
@@ -11,3 +11,13 @@ module.exports = appInfo => {
 
   return config;
 };
+
+exports.alinode = {
+  server: 'http://http://107.191.58.36:7001/',
+  appid: '54171',
+  secret: 'c308e971092ae7d64c2640430c9ecc2ab6fdcf1e',
+  logdir: '/tmp/',
+  error_log: [
+    '/root/.logs/error.#YYYY#-#MM#-#DD#.log'
+  ]
+};
--- config/plugin.js
+++ config/plugin.js
@@ -2,3 +2,7 @@
 
 // had enabled by egg
 // exports.static = true;
+exports.alinode = {
+  enable: true,
+  package: 'egg-alinode'
+};
